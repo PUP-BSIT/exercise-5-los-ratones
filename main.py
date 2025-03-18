574bdb6e1d771fd447df7c438a365e3a5828911c
gonot_jediduncan <jediduncan4@gmail.com>
2025-03-18 22:40:36 +0800

    add question_2 function

--- main.py
+++ main.py
@@ -3,9 +3,28 @@
 def question_1():
     pass
 
-# TODO(Jedi Duncan Gonot): Create question 2.
 def question_2():
-    pass
+    member = "Jedi Duncan S. Gonot"
+    question = "In Valorant, who is the agent that uses knives as their ult?"
+    choices = ["Brimstone", "Waylay", 
+                "Kay-o", "Jett"]
+    right_answer = False
+    
+    print(f"({member}) {question}")
+    print(f"A. {choices[0]} B. {choices[1]}")
+    print(f"C. {choices[2]}  D. {choices[3]}")
+    answer = input("Enter your answer: ").capitalize()
+
+    if answer == 'D':
+        right_answer = True
+        print("You are correct! India is the dirtiest country in the world!")
+    else:
+        print(f"{answer} is incorrect. The correct answer is D.")
+
+    if right_answer == True:
+        return 1
+    else:
+        return 0
 
 # TODO(Dion Manicio): Create question 3.
 def question_3():
@@ -15,29 +34,9 @@ def question_3():
 def question_4():
     pass
 
-
+# TODO(JohnPaul Rodriguez): Create question 5.
 def question_5():
-    member = "John Paul Rodriguez"
-    question = "Which video game features a battle royale mode called 'Warzone'?"
-    choices = ["Fortnite", "Call of Duty", "PUBG", "Apex Legends"]
-    right_answer = False
-
-    print(f"({member}) {question}")
-    print(f"A. {choices[0]} B. {choices[1]}")
-    print(f"C. {choices[2]} D. {choices[3]}")
-    
-    answer = input("Enter your answer: ").capitalize()
-
-    if answer == 'B':
-        right_answer = True
-        print("You are correct! Call of Duty features the Warzone battle royale mode!\n")
-    else:
-        print(f"{answer} is incorrect. The correct answer is B.\n")
-
-    if right_answer == True:
-        return 1
-    else:
-        return 0
+    pass
 
 score = 0
 print("-=-=-LOS RATONES QUIZ-=-=-")
@@ -47,13 +46,13 @@ for x in range(5):
         case 0:
             pass
         case 1:
-            pass
+            score += question_2()
         case 2:
             pass
         case 3:
             pass
         case 4:
-            score += question_5()
+            pass
 
 if score > 1:
     print(f"Congratulations! You scored {score} out of 5!")
