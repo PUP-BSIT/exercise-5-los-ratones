f784a3700b7d1cd7bb5a4dd667217e9a3f9fd108
campos-kenjienishi <kenjicanpos829@gmail.com>
2025-03-18 22:46:31 +0800

    add question 1

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 
-# TODO(Kenji Enishi Campos): Create question 1.
 def question_1():
-    pass
+    member = "Kenji Enishi Campos"
+    question = "In Honkai Star Rail, which one of the following is NOT an Emanator?"
+    choices = ["Madam Herta", "Acheron", "Welt Yang", "Phantylia"]
+    right_answer = False
+
+    print(f"({member}) {question}")
+    print(f"A. {choices[0]}   B. {choices[1]}") 
+    print(f"C. {choices[2]}     D. {choices[3]}")
+    answer = input("Enter your answer: ").capitalize()
+
+    if answer == "C":
+        right_answer = True
+        print("You are correct! Welt Yang is not an Emanator (but he's as strong as them)!\n")
+    else:
+        print(f"{answer} is incorrect. The correct answer is C.\n")
+
+    if right_answer == True:
+        return 1
+    else:
+        return 0
 
 # TODO(Jedi Duncan Gonot): Create question 2.
 def question_2():
@@ -20,12 +38,12 @@ def question_5():
     pass
 
 score = 0
-print("-=-=-LOS RATONES QUIZ-=-=-")
+print("-=-=-LOS RATONES QUIZ-=-=-\n")
 for x in range(5):
     print(f"Question {x+1}:")
     match x:
         case 0:
-            pass
+            score += question_1()
         case 1:
             pass
         case 2:
