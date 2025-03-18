de8539d3b470154ba8f46041becbec3cb9d4256e
reduta-paulbenidict <paulbenidictr@gmail.com>
2025-03-18 22:32:02 +0800

    add main.py

---
+++ main.py
@@ -0,0 +1,41 @@
+
+# TODO(Kenji Enishi Campos): Create question 1.
+def question_1():
+    pass
+
+# TODO(Jedi Duncan Gonot): Create question 2.
+def question_2():
+    pass
+
+# TODO(Dion Manicio): Create question 3.
+def question_3():
+    pass
+
+# TODO(Paul Benidict Reduta): Create question 4.
+def question_4():
+    pass
+
+# TODO(JohnPaul Rodriguez): Create question 5.
+def question_5():
+    pass
+
+score = 0
+print("-=-=-LOS RATONES QUIZ-=-=-")
+for x in range(5):
+    print(f"Question {x+1}:")
+    match x:
+        case 0:
+            pass
+        case 1:
+            pass
+        case 2:
+            pass
+        case 3:
+            pass
+        case 4:
+            pass
+
+if score > 1:
+    print(f"Congratulations! You scored {score} out of 5!")
+else:
+    print("You scored 0 out of 5. Better luck next time!")
